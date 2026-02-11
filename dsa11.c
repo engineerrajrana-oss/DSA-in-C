095e1d28f89bda9d626efffa8011a67da8505672
Harshit Raj Rana <engineer.raj.rana@gmail.com>
2026-02-11 23:58:18 +0530

    dsa11thcode

---
+++ dsa11.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    int A[100][100], B[100][100], C[100][100];
+
+    // Read rows and columns
+    scanf("%d %d", &m, &n);
+
+    // Read first matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Read second matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Add matrices
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Print result matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
