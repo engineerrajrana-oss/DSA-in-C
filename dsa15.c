05dfccd35c182afe0781b65662b3d8ef03ef763b
Harshit Raj Rana <engineer.raj.rana@gmail.com>
2026-02-16 12:05:55 +0530

    14,15,16 dsa daily

---
+++ dsa15.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int sum = 0;
+
+    // Primary diagonal condition: i == j
+    for(int i = 0; i < m && i < n; i++) {
+        sum += matrix[i][i];
+    }
+
+    printf("Sum of primary diagonal = %d\n", sum);
+
+    return 0;
+}
