2b55f099428e0f944decf131fb9de0b20346b3f1
Harshit Raj Rana <engineer.raj.rana@gmail.com>
2026-03-13 23:36:22 +0530

    DAILY 41TH day code

---
+++ 41THDAY.C
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+// Enqueue operation
+void enqueue(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (rear == NULL) {
+        front = rear = newNode;
+        return;
+    }
+
+    rear->next = newNode;
+    rear = newNode;
+}
+
+// Dequeue operation
+void dequeue() {
+    if (front == NULL) {
+        printf("-1\n");
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("%d\n", temp->data);
+
+    front = front->next;
+
+    if (front == NULL)
+        rear = NULL;
+
+    free(temp);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        int choice;
+
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            int value;
+            scanf("%d", &value);
+            enqueue(value);
+        }
+        else if (choice == 2) {
+            dequeue();
+        }
+    }
+
+    return 0;
+}
